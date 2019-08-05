9aae3d462bcb8f457db194ff7ad8338fda8cf269
Calvine Otieno <calvineotieno010@gmail.com>
2019-08-05 15:58:32 +0300

    second commit

--- comments/forms.py
+++ comments/forms.py
@@ -4,18 +4,18 @@ from django.core.exceptions import ValidationError
 
 from .models import Post, CommentPost
 
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ('content', )
-#     def clean_content(self):
-#         data = self.cleaned_data.get('content')
-#         if len(data) <= 10:
-#             raise forms.ValidationError('Please enter at least 10 characters')
-#         return data
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('content', )
+    def clean_content(self):
+        data = self.cleaned_data.get('content')
+        if len(data) <= 10:
+            raise forms.ValidationError('Please enter at least 10 characters')
+        return data
 
-# class CommentPostForm(forms.ModelForm):
+class CommentPostForm(forms.ModelForm):
     
-#     class Meta:
-#         model = CommentPost
-#         fields = ('title', 'text',)
+    class Meta:
+        model = CommentPost
+        fields = ('title', 'text',)
